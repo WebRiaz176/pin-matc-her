e96f88375704d6dd81f18bb7977e8c05c8cc992b
webriaz176 <mohammadriaz2206@gmail.com>
2023-01-25 23:55:30 +0600

    complete

--- js/pin.js
+++ js/pin.js
@@ -19,3 +19,52 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const displayedPin = document.getElementById('display-pin');
     displayedPin.value = pin;
 })
+
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+
+    const typeNumberField = document.getElementById('typed-numbers');
+    const previousTypeNumber = typeNumberField.value;
+
+    if(isNaN(number)){
+        if(number === 'C'){
+            typeNumberField.value = '';
+        }
+
+        else if(number === '<'){
+            const digit = previousTypeNumber.split('');
+            digit.pop();
+            const remainingDigit = digit.join('');
+            typeNumberField.value = remainingDigit;
+        }
+    }
+
+    
+    else{
+        const newTypeNumber = previousTypeNumber + number;
+        typeNumberField.value = newTypeNumber;
+    }
+    
+})
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const generatePinField = document.getElementById('display-pin');
+    const currentPin = generatePinField.value;
+
+    const typePinField = document.getElementById('typed-numbers');
+    const typeNumber = typePinField.value;
+
+    const pinSuccessMessage = document.getElementById('pin-success');
+    const pinFailMessage = document.getElementById('pin-failure');
+
+    if(typeNumber === currentPin){
+        pinSuccessMessage.style.display = 'block';
+        pinFailMessage.style.display = 'none';
+    }
+
+    else{
+        pinSuccessMessage.style.display = 'none';
+        pinFailMessage.style.display = 'block';
+    }
+})
